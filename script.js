448e7d7ff7b144161ac7c77f1462cd1eb8c02b78
Satyata Dhargawe <64574608+satyata@users.noreply.github.com>
2021-05-23 11:19:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -27,4 +27,28 @@ window.addEventListener("scroll", () => {
   }
 });
 
+// Scroll To
+const links = [...document.querySelectorAll(".scroll-link")];
+links.map(link => {
+  link.addEventListener("click", e => {
+    e.preventDefault();
 
+    const id = e.target.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+    const fixNav = nav.classList.contains("fix-nav");
+    let position = element.offsetTop - navHeight;
+
+    if (!fixNav) {
+      position = position;
+    }
+
+    window.scrollTo({
+      top: position,
+      left: 0,
+    });
+
+    menu.classList.remove("show");
+  });
+});
+
+AOS.init();
